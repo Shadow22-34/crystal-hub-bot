9f92b5642a6503494bdee69ceecf92d2d2c9970c
Shadow22-34 <katem2274@outlook.com>
2025-02-18 13:46:43 -0500

    Delete cogs directory

--- cogs/scripts.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-import discord
-
-class ScriptManagement(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    
-    async def setup(bot):
-        await bot.add_cog(ScriptManagement(bot))
