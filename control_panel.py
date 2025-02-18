1333c95c1cab7ededfa3e43b96185fdbb2f70779
Shadow22-34 <katem2274@outlook.com>
2025-02-18 13:38:38 -0500

    Update control_panel.py

--- control_panel.py
+++ control_panel.py
@@ -1,9 +1,1 @@
-import discord
-import io
-from . import bot, hwid_data, check_premium
-from .integration import AutoIntegration
 
-class EnhancedControlPanel(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-        self.integration = AutoIntegration(bot)
